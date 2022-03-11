9a7015328f2d00cb1f0254f6d9d2f544e1da772c
CrustPizza <53199927+CrustPizza@users.noreply.github.com>
2022-03-11 10:24:31 +0900

    Update main.cpp

--- ReflectionProject/main.cpp
+++ ReflectionProject/main.cpp
@@ -11,42 +11,108 @@ typedef ObjectA* (*CreateObjA)();
 typedef ObjectB* (*CreateObjB)();
 typedef void(*callFunc)(ObjectA*);
 
-int main()
+#define asd\
+
+
+class A
 {
-	while (true)
+public:
+	size_t GetTypeHash();
+public:
+	A() : x(100), y(100) {};
+
+	void print(int* _x, int* _y)
 	{
-		if (_kbhit() != 0)
-		{
-			char input = _getch();
+		std::cout << "Print" << std::endl;
+		std::cout << "Print" << std::endl;
 
-			if (input == 'q')
-				break;
+		std::cout << _x << std::endl;
+		std::cout << _y << std::endl;
 
-			HMODULE hDll = LoadLibrary(_T("../Debug/MyDLL.dll"));
-
-			if (hDll != nullptr)
-			{
-				PrintTest temp = reinterpret_cast<PrintTest>(GetProcAddress(hDll, "Print"));
-
-				CreateObjA testa = reinterpret_cast<CreateObjA>(GetProcAddress(hDll, "CreateObjectA"));
-				CreateObjB testb = reinterpret_cast<CreateObjB>(GetProcAddress(hDll, "CreateObjectB"));
-				callFunc test = reinterpret_cast<callFunc>(GetProcAddress(hDll, "CallTest"));
-
-				ObjectA* tempA = testa();
-				ObjectB* tempB = testb();
-
-				temp();
-				
-				test(tempA);
-				test(reinterpret_cast<ObjectA*>(tempB));
-
-				delete tempA;
-				delete tempB;
-
-				FreeLibrary(hDll);
-			}
-		}
+		*_x = 1000;
+		*_y = 1000;
 	}
 
+	void print2()
+	{
+		std::cout << "Print" << std::endl;
+		std::cout << "Print" << std::endl;
+	}
+
+	void print3()
+	{
+		std::cout << "Print" << std::endl;
+		std::cout << "Print" << std::endl;
+	}
+
+	void print4()
+	{
+		std::cout << "Print" << std::endl;
+		std::cout << "Print" << std::endl;
+	}
+
+	int x;
+	int y;
+};
+
+int main()
+{
+	A a;
+
+	void(__thiscall A::*temp3)(int*, int*) = &A::print;
+	void(__thiscall * test3)(int*, int*) = reinterpret_cast<void(__thiscall *&)(int*, int*)>(temp3);
+
+	std::cout << &a.x << std::endl;
+	std::cout << &a.y << std::endl;
+
+	test3(&(a.x), &(a.y));
+
+	std::cout << a.x << std::endl;
+	std::cout << a.y << std::endl;
+
+	//while (true)
+	//{
+	//	if (_kbhit() != 0)
+	//	{
+	//		char input = _getch();
+
+	//		if (input == 'q')
+	//			break;
+
+	//		HMODULE hDll = LoadLibrary(_T("../Debug/MyDLL.dll"));
+
+	//		if (hDll != nullptr)
+	//		{
+	//			PrintTest temp = reinterpret_cast<PrintTest>(GetProcAddress(hDll, "Print"));
+
+	//			CreateObjA testa = reinterpret_cast<CreateObjA>(GetProcAddress(hDll, "CreateObjectA"));
+	//			CreateObjB testb = reinterpret_cast<CreateObjB>(GetProcAddress(hDll, "CreateObjectB"));
+	//			callFunc test = reinterpret_cast<callFunc>(GetProcAddress(hDll, "CallTest"));
+
+	//			ObjectA* tempA = testa();
+	//			ObjectB* tempB = testb();
+
+	//			temp();
+	//			
+	//			test(tempA);
+	//			test(reinterpret_cast<ObjectA*>(tempB));
+
+	//			delete tempA;
+	//			delete tempB;
+
+	//			FreeLibrary(hDll);
+	//		}
+	//	}
+	//}
+
 	return 0;
+}
+
+size_t A::GetTypeHash()
+{
+	static size_t uP;
+
+	x = 1000;
+
+	return reinterpret_cast<size_t>(&uP);
 }
